c77a6c4f3e0cecc01e49516f7e321f1467fd52d4
Brittanie Joseph <brittanie.joseph03@gmail.com>
2022-12-10 10:52:26 -0500

    first commit

--- src/components/EditTask.jsx
+++ src/components/EditTask.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from 'react-bootstrap/Button';
 
@@ -7,23 +7,23 @@ function EditTask({editContent, setEditContent, tasks, setTasks}) {
   const onSave = (e, id) => {
     e.preventDefault();
 
-    
+    updateTask(editContent, id) 
   }
 
-  const updateTaskObj = (editcontent, id) => {
+  const updateTask = (editContent, id) => {
+    const updatedTaskObj = tasks.map(taskObj => {
+      if (taskObj.id === id) {
+        console.log('editContent ln 16 @updateTask fn', editContent)
+        return {editContent, id}
+      }
+      console.log(taskObj, "taskObj @ updatedTaskObj")
+      console.log("save edited task", taskObj)
+      console.log(editContent, "editContent state")
+    })
+    return updatedTaskObj
 
-
-  }
-  const updatedTaskObj = tasks.map(taskObj => {
-    if (taskObj.id === id) {
-      return {editContent, id}
-    }
-    console.log(taskObj, "taskObj @ updatedTaskObj")
-    console.log("save edited task", taskObj)
-    console.log(editContent, "editContent state")
     // setTasks(updatedTaskObj)
-    
-  })
+  }
 
   return (
     <div>
